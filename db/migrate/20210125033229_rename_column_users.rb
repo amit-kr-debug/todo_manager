8168e667a0a6b04f71b70dc591a31d63191dd92b
Amit <amitkr0819@gmail.com>
2021-01-26 12:33:43 +0530

    trying to fix db migration on heroku

--- db/migrate/20210125033229_rename_column_users.rb
+++
@@ -1,7 +0,0 @@
-class RenameColumnUsers < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :users, :name, :first_name
-    rename_column :users, :password, :password_digest
-    add_timestamps(:users)
-  end
-end
---
+++ db/migrate/20210126065642_drop_todos.rb
@@ -0,0 +1,4 @@
+class DropTodos < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
